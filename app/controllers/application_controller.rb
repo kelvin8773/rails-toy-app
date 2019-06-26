714ff9c9892b073361ab525eda1565d1ccf9cd9e
Kelvin Liang <kelvin9877@gmail.com>
2019-06-26 23:38:43 +0800

    try to update hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello, World!"
+    render html: "¡¡¡Hello, World!"
   end
   
 end
